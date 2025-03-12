ef332eb152507d99cb905e27502040ead2223163
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-03-12 11:54:01 -0700

    wip: required and mounted passed

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 
@@ -27,6 +29,9 @@ app.use(
     saveUninitialized: true,
   })
 );
+
+//passUserToView comes after session middleware but before homepage
+app.use(passUserToView);
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {
